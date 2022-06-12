16281e2c5ef41253e4717073f08f48e7c7c56dd9
Amirhosein Heidari <amirhosein5858@gmail.com>
2022-06-12 23:38:21 +0430

    init project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import createEmotionCache from "../utility/createEmotionCache";
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ThemeDark } from "../styles/themes/theme.dark";
+import { ThemeLight } from "../styles/themes/theme.light";
+import "../styles/globals.css";
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={ThemeDark}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
---
+++ styles/themes/theme.light.ts
@@ -0,0 +1,8 @@
+import { createTheme } from '@mui/material/styles';
+
+export const ThemeLight = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
